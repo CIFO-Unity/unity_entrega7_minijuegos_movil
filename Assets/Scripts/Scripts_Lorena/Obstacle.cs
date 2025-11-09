446370fa4fa18b7ba682e90215291d54574f14d3
veggiemania <milkcowgames@gmail.com>
2025-11-09 19:49:08 +0100

    Eliminado el método OnTriggerEnter2D de Obstacle porque no tenía utilidad

--- Assets/Scripts/Scripts_Lorena/Obstacle.cs
+++ Assets/Scripts/Scripts_Lorena/Obstacle.cs
@@ -19,15 +19,6 @@ public class Obstacle : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            var player = other.GetComponent<PlayerController>();
-            if (player != null) player.Die();
-        }
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player"))
